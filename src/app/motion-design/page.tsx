c434324779a574e2124b882ea778b64f5e5de695
PROSPER ABAYOMI <pablocodes3846@gmail.com>
2026-05-04 11:25:19 +0100

    try 2

--- src/app/motion-design/page.tsx
+++ src/app/motion-design/page.tsx
@@ -11,20 +11,21 @@ import countdownTimerImg from "../../../public/assets/images/countdown-timer.png
 import textAnimationImg from "../../../public/assets/images/text-animation.png";
 
 import driveIcon from "../../../public/assets/icons/drive.svg";
+
 type Theme = "dark" | "light";
 const MotionDesignPage = () => {
-          const [theme, setTheme] = useState<Theme>("dark");
-        
-          // 1. Theme Persistence Logic
-          useEffect(() => {
-            const savedTheme =
-              (localStorage.getItem("theme") as Theme | null) || "dark";
-            setTheme(savedTheme);
-          }, []);
-        
-          useEffect(() => {
-            localStorage.setItem("theme", theme);
-          }, [theme]);
+  const [theme, setTheme] = useState<Theme>("dark");
+
+  // 1. Theme Persistence Logic
+  useEffect(() => {
+    const savedTheme =
+      (localStorage.getItem("theme") as Theme | null) || "dark";
+    setTheme(savedTheme);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
   return (
     <div className="pt-8 mx-[5%]">
       <Header theme={theme} setTheme={setTheme} />
