acf61654d9403270117cb48dd1b3d33f04eda0f2
PROSPER ABAYOMI <pablocodes3846@gmail.com>
2026-06-01 00:00:48 +0100

    playground quicklink connected

--- src/app/components/Works.tsx
+++ src/app/components/Works.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-// import PasswordProtectedModal from "./PasswordProtectedModal";
+import PasswordProtectedModal from "./PasswordProtectedModal";
 import ComingSoon from "./ComingSoon";
 
 type Theme = "dark" | "light";
@@ -162,14 +162,14 @@ const Works = ({ theme }: WorksProps) => {
           ))}
         </div>
       </div>
-{/*       
+      
       <PasswordProtectedModal
         isOpen={passwordModalOpen}
         onClose={() => setPasswordModalOpen(false)}
         projectTitle={selectedProject?.title || ""}
         projectImage={selectedProject?.image || ""}
         projectHref={selectedProject?.href || ""}
-      /> */}
+      />
 
       <ComingSoon
         isOpen={comingSoonModalOpen}
